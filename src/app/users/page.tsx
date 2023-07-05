e40e110f692bea2267e7657cb738521a1b0d674e
Your Name <you@example.com>
2023-07-05 12:48:18 +0100

    made modifications to type.dts and library.

---
+++ src/app/users/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import getAllUsers from "../../../lib/getAllUsers"
+ 
+export default async function Users() {
+     const usersData : Promise<Users[]>= getAllUsers()
+     const users = await usersData
+     
+  return (
+    <>
+    <div>
+        <h1 className="text-white text-lg text-center">getAllUsers</h1>
+  {users.map(user=>{
+    return(
+        <p className="text-white text-lg text-center" key={user.id}>
+          <Link href={`/users/${user.id}`} >{user.name}</Link>
+        </p>
+    )
+  })}
+    </div></>
+  )
+}
